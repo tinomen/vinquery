665227e148545bbc9988ab3f48fed16dc3b1a09b
Jake Mallory <tinomen@gmail.com>
2011-08-03 19:09:18 -0600

    updated url to include vehicle type and gvwr parameters

--- lib/vinquery.rb
+++ lib/vinquery.rb
@@ -1,30 +1,38 @@
 require 'net/http'
 require 'nokogiri'
+require 'Logger'
 
 class Vinquery
   attr_reader :attributes, :errors, :result
 
   def self.get(vin, options={})
-    request = Vinquery.new(options[:url], options[:access_code], options[:report_type])
+    request = Vinquery.new(options[:url], options[:access_code], options[:report_type], options[:logger])
     doc = request.fetch vin
     result = request.parse doc
     request
   end
 
-  def initialize(url, access_code, report_type)
+  def initialize(url, access_code, report_type, log = Logger.new(nil))
     @url = url
     @access_code = access_code
     @report_type = report_type
+    @log = log
   end
   
   def fetch(vin)
     # use reducable=FALSE to get additional fields like fuel_type
-    @uri ||= "#{@url}?accessCode=#{@access_code}&reportType=#{@report_type}&reducable=FALSE&vt=true&gvwr=true"
+    # use vt=true to get vehicle_type
+    # http://www.vinquery.com/ws_POQCXTYNO1D/xml_v100_QA7RTS8Y.aspx?accessCode=6958785b-ac0a-4303-8b28-40e14aa836ce&vin=YourVINToDecode&reportType=0&vt=true&gvwr=true
+    @uri ||= "#{@url}?accessCode=#{@access_code}&reportType=#{@report_type}&reducable=FALSE&vt=TRUE&gvwr=TRUE"
     url_s = @uri + "&vin=#{vin}"
+    @log.info{"Vinquery#fetch - uri: #{url_s}"}
     url = URI.parse(url_s)
     begin
       @result = Net::HTTP.get url
+      @log.debug{"Vinquery#fetch - result: #{@result}"}
     rescue Exception => e
+      @log.error{"ERROR - #{e.message}"}
+      @log.error{"ERROR - #{e.backtrace[0]}"}
       xml = Nokogiri::XML::Builder.new do |doc|
         doc.vin(:number => vin,:status => "FAILED") {
           doc.message(:Key => "VinQuery unavailable", :Value => "Oops, it looks like our VIN decoder is unavailable at the moment. Please try again later.")
@@ -46,9 +54,10 @@ class Vinquery
     doc.xpath('//vehicle[1]/item').each do |item|
       attributes[item.attributes['key'].value.downcase.gsub('.', '').gsub(/ /, '_').to_sym] = item.attributes['value'].value
     end
+    @log.info{"Vinquery#set_attributes - number of attributes parsed: #{attributes.size}"}
     if attributes.size > 0
-      # attributes[:vin] = doc.css('vin').first.attributes['number'].value
-      attributes[:vin_key] = make_vin_key(attributes[:vin])
+      vin = doc.css('vin').first.attributes['number'].value
+      attributes[:vin_key] = make_vin_key(vin)
       attributes[:vendor_result] = doc.to_xml 
     end
     @attributes = attributes
@@ -57,7 +66,8 @@ class Vinquery
   def set_errors_hash(doc)
     @errors = []
     @valid = doc.css('vin').first.attributes['status'].value == "SUCCESS"
-    doc.css('message').each{|msg| @errors << {msg.attributes['key'].value => msg.attributes['value'].value} } unless @valid
+    doc.css('message').each{|msg| @errors << {msg.attributes['key'].value => msg.attributes['value'].value} } unless valid?
+    @errors.each{|msg| @log.error{"Vinquery#set_errors_hash - error: #{msg.to_s}"}} unless @errors.empty
   end
 
   def valid?
